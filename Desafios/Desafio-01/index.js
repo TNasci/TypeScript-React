b9ac6534b1728f82eebb2e218f0c06a05fb71c36
Thiago Araujo Nascimento <50850460+TNasci@users.noreply.github.com>
2022-11-04 17:53:30 -0300

    Meu Primeiro Desafio

---
+++ Desafios/Desafio-01/index.js
@@ -0,0 +1,7 @@
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(2, 2));
+var z = 5;
+var y = 8;
+console.log(soma(z, y));
---
+++ Desafios/Desafio-01/index.ts
@@ -0,0 +1,10 @@
+function soma(a: number, b: number) {
+    return a + b
+}
+
+console.log(soma(2, 2))
+
+const z = 5
+const y = 8
+
+console.log(soma(z, y))
