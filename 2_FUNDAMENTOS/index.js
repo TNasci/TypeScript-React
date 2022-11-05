78891077b37827629882b80b7c6c1077b034fbe5
Thiago Araujo Nascimento <50850460+TNasci@users.noreply.github.com>
2022-11-05 07:28:25 -0300

    Adicionando a sessão fundamentos

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,17 @@
+// 1- numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.584848;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+// 2 - string
+var firstName = "Thiago";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Nascimento";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,25 @@
+// 1- numbers
+let x: number = 10
+
+console.log(x)
+x =16
+console.log(typeof x)
+
+const y = 15.584848
+console.log(typeof y )
+console.log(y)
+console.log(y.toPrecision(3))
+
+// 2 - string
+
+const firstName: string = "Thiago"
+
+console.log(firstName.toUpperCase())
+let fullName: string
+
+const lastName: string = "Nascimento"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+console.log(typeof fullName)
