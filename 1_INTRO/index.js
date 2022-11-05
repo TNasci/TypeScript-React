7956960a554924bcf772695fcf593c1148be0575
Thiago Araujo Nascimento <50850460+TNasci@users.noreply.github.com>
2022-11-05 07:30:37 -0300

    Ajustes do projeto

---
+++ 1_INTRO/index.js
@@ -0,0 +1,7 @@
+var firstName = "Thiago";
+var anotherName = "Nascimento";
+var x = true;
+function greating(name) {
+    console.log("Olá " + name);
+}
+greating(firstName);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,10 @@
+const firstName = "Thiago"
+const anotherName = "Nascimento"
+const x = true
+
+function greating(name: string) {
+    console.log("Olá " + name)
+
+}
+
+greating(firstName)
